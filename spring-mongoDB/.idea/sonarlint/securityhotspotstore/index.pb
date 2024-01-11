c7118a675fe0877d3df2a50182adec1c44841c35
blackbean <ymecca730135@gmail.com>
2024-01-12 01:52:45 +0900

    [feat] : MongoDB UTC Time fixed problem fix

--- spring-mongoDB/.idea/sonarlint/securityhotspotstore/index.pb
+++ spring-mongoDB/.idea/sonarlint/securityhotspotstore/index.pb
@@ -8,3 +8,7 @@ Esrc/main/java/com/example/springmongodb/SpringMongoDbApplication.java,0/f/0f80
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+y
+Isrc/main/java/com/example/springmongodb/MongoClientConnectionExample.java,1/2/12cd79cd71bef8bf6c6765132f39f7cf63d970f2
+4
+.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
--- spring-mongoDB/src/main/java/com/example/springmongodb/SpringMongoDbApplication.java
+++ spring-mongoDB/src/main/java/com/example/springmongodb/SpringMongoDbApplication.java
@@ -1,12 +1,43 @@
 package com.example.springmongodb;
 
+import com.example.springmongodb.domain.Applicant;
+import com.example.springmongodb.domain.ApplicantMongoDBRepository;
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMongoDbApplication {
+@RequiredArgsConstructor
+public class SpringMongoDbApplication implements CommandLineRunner {
+  private final ApplicantMongoDBRepository repository;
 
   public static void main(String[] args) {
     SpringApplication.run(SpringMongoDbApplication.class, args);
+  }
+
+  @PostConstruct
+  public void started() {
+    // timezone UTC 셋팅
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
+  @Override
+  public void run(String... args) throws Exception {
+    Map<String, Object> qna = new HashMap<>();
+    qna.put("year", 24);
+    qna.put("name", "이서현");
+    qna.put("니가 이 문제를 풀 수 있을까?", "아니요");
+    qna.put("당신은 누구입니까?", "이서현2");
+    qna.put("temporary", "temporary");
+    qna.put("date", LocalDateTime.now());
+    repository.save(new Applicant("2", 2021, qna));
+    List<Applicant> byYear = repository.findByYear(2021);
+    System.out.println(byYear.get(0).getYear());
   }
 }
